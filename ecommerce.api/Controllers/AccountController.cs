bd5b6c75c9719b29207882cc63f1053d86f02a70
Serkan TOY <stoy@windowslive.com>
2026-05-08 22:53:56 +0300

    ApiCoreController eklendi (CheckEmailExistsAsync,CheckNameExistsAsync)

--- ecommerce.api/Controllers/AccountController.cs
+++ ecommerce.api/Controllers/AccountController.cs
@@ -1,6 +1,5 @@
 ﻿using ecommerce.api.Models.Domain.DTOs.Account;
 using ecommerce.api.Models.Domain.Entities.Users;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,19 +10,56 @@ namespace ecommerce.api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AccountController : ControllerBase
-    {
-        private UserManager<UserApp> userManager;
-        public AccountController(UserManager<UserApp> userManager)
-        {
-            this.userManager = userManager;
-        }
+    public class AccountController : ApiCoreController
+    {      
 
-        [HttpPost("register")]
-        public async Task<IActionResult> Register(RegisterDto registerDto)
+        [HttpPost]
+        [ActionName("login")]
+        public async Task<IActionResult> Login(RegisterDto registerDto)
         {
             var user = await userManager.Users.Where(u => u.UserName == registerDto.Email).FirstOrDefaultAsync();
-            return Ok("Register successful");
+            IdentityResult result = await userManager.CreateAsync(new UserApp
+            {
+                Email = registerDto.Email,
+                Name = registerDto.Name,
+                SurName = registerDto.SurName,
+                UserName = registerDto.Email,
+            });
+
+            if (result.Succeeded)
+            {
+                return Ok("Register successful");
+            }
+            return BadRequest("Register failed");
+        }
+
+        [HttpPost]
+        [ActionName("register")]
+        public async Task<IActionResult> Register(RegisterDto registerDto)
+        {
+            if(await CheckEmailExistsAsync(registerDto.Email))
+            {
+                return BadRequest("Email adresi kayıtlı.");
+            }
+
+            if (await CheckEmailExistsAsync(registerDto.Email))
+            {
+                return BadRequest("Username kayıtlı.");
+            }
+
+            IdentityResult result = await userManager.CreateAsync(new UserApp
+                    {
+                        Email = registerDto.Email,
+                        Name = registerDto.Name,
+                        SurName = registerDto.SurName,
+                        UserName = registerDto.Email,
+                    });
+
+            if (result.Succeeded)
+            {
+                return Ok("Register successful");
+            }
+            return BadRequest("Register failed");
         }
     }
 }
---
+++ ecommerce.api/Controllers/ApiCoreController.cs
@@ -0,0 +1,31 @@
+﻿using ecommerce.api.Models.Domain.Entities.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace ecommerce.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiCoreController : ControllerBase
+    {
+        protected UserManager<UserApp> userManager;
+        public ApiCoreController(UserManager<UserApp> userManager = null)
+        {
+            this.userManager = userManager;
+        }
+
+
+        protected async Task<bool> CheckEmailExistsAsync(string email)
+        {
+            return await userManager.Users.AnyAsync(u => u.Email == email);
+        }
+
+
+        protected async Task<bool> CheckNameExistsAsync(string username)
+        {
+            return await userManager.Users.AnyAsync(u => u.UserName == username);
+        }
+    }
+}
